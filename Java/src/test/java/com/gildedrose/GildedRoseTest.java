0ed2d949b79d6668dbb439ca3e4c2819bac38fb7
Edward Kleszcz <edward.kleszcz@bjss.com>
2022-04-07 16:36:38 +0100

    tests for brie

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -5,39 +5,70 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
-
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    private GildedRose addAndUpdateApp(Item[] products) {
+        Item[] items = products;
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+        return app;
+    }
+
+    private int getQuality(GildedRose app, int index) {
+        return app.items[index].quality;
     }
 
     @Test
+    void foo() {
+        GildedRose app = addAndUpdateApp(new Item[]{new Item("foo", 10, 10)});
+        assertEquals("foo", app.items[0].name);
+    }
+
+
+    @Test
     void givenStandardItemBeforeSellIn_returnsDecrementedValues() {
-        Item[] items = new Item[] { new Item("foo", 10, 10)};
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals(9, app.items[0].quality);
+        GildedRose app = addAndUpdateApp(new Item[]{new Item("foo", 10, 10)});
+        assertEquals(9, getQuality(app,0));
         assertEquals(9, app.items[0].sellIn);
     }
     @Test
     void givenStandardItemAfterSellIn_returnDecrementedValues() {
-        Item[] items = new Item[] { new Item("foo", 0, 10)};
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals(8, app.items[0].quality);
+        GildedRose app = addAndUpdateApp(new Item[]{new Item("foo", 0, 10)});
+
+        assertEquals(8, getQuality(app,0));
         assertEquals(-1, app.items[0].sellIn);
     }
 
     @Test
     void givenLowQualityStandardItem_returnsZeroQuality() {
-        Item[] items = new Item[] { new Item("foo", 10, 0), new Item("bar", -1, 1)};
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals(0, app.items[0].quality);
-        assertEquals(0, app.items[1].quality);
+        Item[] items = new Item[] {new Item("foo", 10, 0), new Item("bar", -1, 1)};
+        GildedRose app = addAndUpdateApp(items);
+
+        assertEquals(0, getQuality(app,0));
+        assertEquals(0, getQuality(app,1));
     }
+
+    @Test
+    void givenAgedBrie_returnsIncreasedQuality() {
+        Item[] items = new Item[] {new Item("Aged Brie", 10, 10)};
+        GildedRose app = addAndUpdateApp(items);
+
+        assertEquals(11, getQuality(app,0));
+    }
+
+    @Test
+    void givenHighQualityBree_returnMaxQuality() {
+        GildedRose app = addAndUpdateApp(new Item[]{new Item("Aged Brie", 10, 49), new Item("Aged Brie", 10, 50)});
+
+        assertEquals(50, getQuality(app,0));
+        assertEquals(50, getQuality(app,1));
+    }
+
+    @Test
+    void givenAgedBrieAfterSellIn_returnQualityIncreasedBy2() {
+        GildedRose app = addAndUpdateApp(new Item[]{new Item("Aged Brie", 0, 10)});
+
+        assertEquals(12, getQuality(app,0));
+    }
+
+
 
 }
