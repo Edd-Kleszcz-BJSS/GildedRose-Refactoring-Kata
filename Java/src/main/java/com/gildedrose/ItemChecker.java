290b7b51bc46838e17e87d62c699258e5561338c
Edward Kleszcz <edward.kleszcz@bjss.com>
2022-04-07 22:13:36 +0100

    refactoring

--- Java/src/main/java/com/gildedrose/ItemChecker.java
+++ Java/src/main/java/com/gildedrose/ItemChecker.java
@@ -2,27 +2,23 @@ package com.gildedrose;
 
 public class ItemChecker {
     public static void areValidItems(Item[] items) throws RuntimeException {
-        containsItems(items);
-        hasCompleteItems(items);
+        containsValidItems(items);
         allHaveValidQuality(items);
     }
 
-    private static void containsItems(Item [] items) throws NullPointerException {
+    private static void containsValidItems(Item [] items) throws NullPointerException {
         if (items == null) {
             throw new NullPointerException("Items must be fully defined");
         } else if (items.length == 0) {
             throw new NullPointerException("Items empty");
         }
-    }
-
-    private static void hasCompleteItems(Item[] items) {
         for (int index = 0; index < items.length; index++) {
             if (items[index].name == null) {
                 throw new NullPointerException("Items must be fully defined");
             }
         }
     }
-
+    
     private static void allHaveValidQuality(Item[] items) throws RuntimeException {
         for (int index = 0; index < items.length; index++) {
             if (items[index].name.equals("Sulfuras, Hand of Ragnaros")) {
