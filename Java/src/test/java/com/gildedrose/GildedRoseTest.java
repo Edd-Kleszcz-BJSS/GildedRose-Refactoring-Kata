222a4a4b30af439fc6e682cecbe5a2a828b2a857
Edward Kleszcz <edward.kleszcz@bjss.com>
2022-04-07 16:18:33 +0100

    new tests for standard items

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -11,7 +11,33 @@ class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
+    }
+
+    @Test
+    void givenStandardItemBeforeSellIn_returnsDecrementedValues() {
+        Item[] items = new Item[] { new Item("foo", 10, 10)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(9, app.items[0].quality);
+        assertEquals(9, app.items[0].sellIn);
+    }
+    @Test
+    void givenStandardItemAfterSellIn_returnDecrementedValues() {
+        Item[] items = new Item[] { new Item("foo", 0, 10)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(8, app.items[0].quality);
+        assertEquals(-1, app.items[0].sellIn);
+    }
+
+    @Test
+    void givenLowQualityStandardItem_returnsZeroQuality() {
+        Item[] items = new Item[] { new Item("foo", 10, 0), new Item("bar", -1, 1)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].quality);
+        assertEquals(0, app.items[1].quality);
     }
 
 }
